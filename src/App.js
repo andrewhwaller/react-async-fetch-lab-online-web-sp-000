0adc2d387db39d1bcad407c671b5002f69c5b2fb
Andrew Waller <andrewhwaller@gmail.com>
2019-09-27 19:36:46 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React, {Component} from 'react';
 
 class App extends Component {
 
-
+  componentDidMount() {
+    fetch('')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          appData: data
+        })
+      })
+  }
   render() {
 
   }
