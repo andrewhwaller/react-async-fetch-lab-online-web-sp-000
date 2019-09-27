98d7e5e64d67c3741be8605333b1b13541138818
Andrew Waller <andrewhwaller@gmail.com>
2019-09-27 19:37:29 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, {Component} from 'react';
 
 class App extends Component {
 
+  state = {
+    appData: []
+  }
   componentDidMount() {
     fetch('')
       .then(response => response.json())
